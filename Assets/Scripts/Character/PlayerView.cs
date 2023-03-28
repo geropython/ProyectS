717249c1562851c3c677fe407528311c9ed752f1
Jalil <jputer@uade.edu.ar>
2023-03-28 18:34:13 -0300

    Idle animation added

--- Assets/Scripts/Character/PlayerView.cs
+++ Assets/Scripts/Character/PlayerView.cs
@@ -10,20 +10,20 @@ public class PlayerView : MonoBehaviour
     
     private static readonly int XDirection = Animator.StringToHash("XDirection");
     private static readonly int YDirection = Animator.StringToHash("YDirection");
-    private static readonly int Diagonal = Animator.StringToHash("Diagonal");
-
+    private static readonly int Idle = Animator.StringToHash("Idle");
+    
     private Vector2 _viewDirection;
+    
 
     private void Update()
     {
-        if (playerModel.Direction.x == 0 && playerModel.Direction.y == 0) return;
+        var idle = playerModel.Direction.x == 0 && playerModel.Direction.y == 0;
+        animator.SetBool(Idle, idle);
+        if (idle) return;
         
         animator.SetFloat(XDirection, playerModel.Direction.x);
         animator.SetFloat(YDirection, playerModel.Direction.y);
-    }
-
-    private void SetAnimation()
-    {
         
     }
+
 }
