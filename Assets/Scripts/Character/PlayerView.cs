dd88cbe9649497d2d2510d109043eedb2f59e615
Jalil <jputer@uade.edu.ar>
2023-04-11 19:54:03 -0300

    Controller WIP

--- Assets/Scripts/Character/PlayerView.cs
+++ Assets/Scripts/Character/PlayerView.cs
@@ -7,22 +7,34 @@ public class PlayerView : MonoBehaviour
 
     [SerializeField] private PlayerModel playerModel;
     [SerializeField] private Animator animator;
+    [SerializeField] private InputController _inputController;
     
     private static readonly int XDirection = Animator.StringToHash("XDirection");
     private static readonly int YDirection = Animator.StringToHash("YDirection");
     private static readonly int Idle = Animator.StringToHash("Idle");
     
     private Vector2 _viewDirection;
-    
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
 
     private void Update()
     {
         var idle = playerModel.Direction.x == 0 && playerModel.Direction.y == 0;
         animator.SetBool(Idle, idle);
-        if (idle) return;
+
+        var lookAt = (_inputController.MousePosition - (Vector2)transform.position).normalized;
         
-        animator.SetFloat(XDirection, playerModel.Direction.x);
-        animator.SetFloat(YDirection, playerModel.Direction.y);
+        var x = idle ? lookAt.x : playerModel.Direction.x;
+        var y = idle ? lookAt.y : playerModel.Direction.y;
+        
+        Debug.Log($"Animator x:{x} y:{y} ");
+        
+        animator.SetFloat(XDirection, x);
+        animator.SetFloat(YDirection, y);
         
     }
 
--- Assets/Scripts/Input/InputController.cs
+++ Assets/Scripts/Input/InputController.cs
@@ -1,11 +1,32 @@
+using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class InputController : MonoBehaviour
 {
     private Vector2 _movement;
+    private Vector2 _mousePosition;
     public Vector2 Movement => _movement;
+    public Vector2 MousePosition => _mousePosition;
+
+    private Camera _camera;
     
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+
+    private void Update()
+    {
+        
+        _mousePosition = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        
+        // Debug.Log($"Mouse position x:{_lookDirection.x} y:{_lookDirection.y}");
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
         _movement = context.ReadValue<Vector2>();
