37d059c9cae655bb36f316fb4bb3ff94c2d918cc
geropython <72422102+geropython@users.noreply.github.com>
2023-04-18 19:40:00 -0300

    takeDamage

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -4,12 +4,29 @@ namespace Character
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] public int health;
+        [SerializeField] public int maxHealth;
         [SerializeField] private InputController inputController;
         [SerializeField] private PlayerModel playerModel;
 
         private void Update()
         {
             playerModel.Move(inputController.Movement);
+        }
+        private void Start()
+        {
+            health = maxHealth;
+        }
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            Debug.Log("Damage= " + health);
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                Debug.Log("Morido");
+            }
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using static UnityEngine.EventSystems.EventTrigger;
+using static UnityEngine.GraphicsBuffer;
+using Character;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int damage = 15;
+    public PlayerController _controller;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            _controller.TakeDamage(damage);
+        }
     }
 }
