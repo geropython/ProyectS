50175f1018708a6d9c03432c22ef4724fd135b48
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-05-16 13:11:26 -0300

    New enemy and Level Changes - EDTIOR TOOL
    ADDED new Rider Flow Tool
    ADDED New Mutant Enemy animations ( Idle, walk, attack) with sides animations too.
    ADDED new enemy Scripts ( placeholders)
    ADDED new EnemyMutant prefab.
    
    CHANGED Map design
    CHANGED hierarchy sections
    
    Level / world needs rework.

---
+++ Assets/Scripts/Enemies/EnemiesControllers/EnemyController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   //Enemy Basic Controller, movement, and related stuff.
+    
+    
+}
