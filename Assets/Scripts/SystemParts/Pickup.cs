7e3bcc914e44a4e9172c9d4196377a340ebd7e1d
Maxi Perez <55637107+Durzdor@users.noreply.github.com>
2023-06-30 14:17:43 -0300

    ADD Pickup ground icon
    Pickups now show their respective icons while on the ground

--- Assets/Scripts/SystemParts/Pickup.cs
+++ Assets/Scripts/SystemParts/Pickup.cs
@@ -1,6 +1,7 @@
+using System;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(BoxCollider2D), typeof(SpriteRenderer))]
 public class Pickup : MonoBehaviour
 {
     [SerializeField] private ItemSO item;
@@ -10,6 +11,19 @@ public class Pickup : MonoBehaviour
     private void Awake()
     {
         Invoke(nameof(Destruction), lifetime);
+    }
+
+    private void Start()
+    {
+        var spriteR = GetComponent<SpriteRenderer>();
+        spriteR.sprite = item.Icon;
+        var coll = GetComponent<BoxCollider2D>();
+        coll.offset = new Vector2(0, 0);
+        var bounds = spriteR.bounds;
+        var lossyScale = transform.lossyScale;
+        coll.size = new Vector3(bounds.size.x / lossyScale.x,
+            bounds.size.y / lossyScale.y,
+            bounds.size.z / lossyScale.z);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
