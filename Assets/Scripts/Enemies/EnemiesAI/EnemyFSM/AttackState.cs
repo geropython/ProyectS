15c6e8ecaefb19c912d7fd2ca75ee9da448883e5
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-07-11 12:37:58 -0300

    ADDED base EnemyFSM, some UI packs
    NEW BASIC FSM enemy, enemyState, and new Enemy patrol, idle, attack, chase, Death states added.
    
    ADDED new UI packs, health bars, icons, all sliced and set up.

---
+++ Assets/Scripts/Enemies/EnemiesAI/EnemyFSM/AttackState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : EnemyState
+{
+    public AttackState(EnemyFSM fsm) : base(fsm)
+    {
+    }
+    public override void Enter()
+    {
+        // ...
+    }
+
+    public override void Update()
+    {
+        // ...
+    }
+
+    public override void Exit()
+    {
+        // ...
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemiesAI/EnemyFSM/DeathState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : EnemyState
+{
+    
+    public DeathState(EnemyFSM fsm) : base(fsm)
+    {
+    }
+    
+    public override void Enter()
+    {
+        // ...
+    }
+
+    public override void Update()
+    {
+        // ...
+    }
+
+    public override void Exit()
+    {
+        // ...
+    }
+}
