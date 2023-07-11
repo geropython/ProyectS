46f9b4fb7bf98f2e3020574b1cf9bc8ef21a6a1e
Maxi Perez <55637107+Durzdor@users.noreply.github.com>
2023-07-11 14:11:50 -0300

    ADD items to database
    more items, renamed a few, TODO consumable, add tire pixel placeholder

--- Assets/Maxifolder/ItemConsumable.cs
+++ Assets/Maxifolder/ItemConsumable.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Item", menuName = "Scriptables/Item/Consumable", order = 0)]
 public class ItemConsumable : ItemSO
 {
+    // TODO:ConsumeActionScript
+
+    // [SerializeField] private ConsumableAction _consumeAction;
+
     private void OnEnable()
     {
         itemCategories = ItemCategories.Consumable;
+    }
+
+    public void ConsumeItem()
+    {
+        // _consumeAction.Execute();
     }
 }
